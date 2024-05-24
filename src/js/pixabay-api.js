07563c4a9ca2e26172b157f0b845cc106d5bd5b7
AndriyV2504 <154068131+AndriyV2504@users.noreply.github.com>
2024-05-24 21:25:54 +0300

    add-task-11-err

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,10 +2,17 @@ const API_KEY = '43967395-ba8c6739f73e8322e41736fdd';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query) {
-  const response = await fetch(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`);
-  if (!response.ok) {
-    throw new Error('Network response was not ok.');
+  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  try {
+      const response = await fetch(url);
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      return data.hits;
+  } catch (error) {
+      console.error('Fetch error:', error);
+      throw error;
   }
-  const data = await response.json();
-  return data;
 }
