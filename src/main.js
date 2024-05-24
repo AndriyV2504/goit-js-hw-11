73d2cd7ecd21afdc53e05eefaf4359d2cf928d21
AndriyV2504 <154068131+AndriyV2504@users.noreply.github.com>
2024-05-24 23:00:32 +0300

    add-task-11-main

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import { fetchImages } from './js/pixabay-api.js';
+import { renderImages, showNotification, clearGallery, initLightbox, showLoader, hideLoader } from './js/render-functions.js';
+
+document.addEventListener(`DOMContentLoaded`, () => {
+  const form = document.querySelector(`.search-form`);
+  form.addEventListener(`submit`, onSearch);
+  initLightbox();
+});
+
+async function onSearch(event) {
+  event.preventDefault();
+
+  const query = event.target.elements.searchQuery.value.trim();
+  if (!query) {
+    showNotification(`Please enter a search query!`, `warning`);
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const images = await fetchImages(query);
+    hideLoader();
+
+    if (images.length === 0) {
+      showNotification(`Sorry, there are no images matching your search query. Please try again!`, `warning`);
+      return;
+    }
+
+    renderImages(images);
+  } catch (error) {
+    hideLoader();
+    showNotification(`An error occurred while fetching images. Please try again!`, `error`);
+  }
+}
