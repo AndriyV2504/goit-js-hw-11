b5b136b58766c970882692b788a40a12604c03e8
AndriyV2504 <154068131+AndriyV2504@users.noreply.github.com>
2024-05-27 15:33:07 +0300

    add-task-11

--- src/main.js
+++ src/main.js
@@ -2,35 +2,36 @@ import { fetchImages } from './js/pixabay-api.js';
 import { renderImages, showNotification, clearGallery, initLightbox, showLoader, hideLoader } from './js/render-functions.js';
 
 document.addEventListener(`DOMContentLoaded`, () => {
-  const form = document.querySelector(`.search-form`);
-  form.addEventListener(`submit`, onSearch);
-  initLightbox();
+    const form = document.querySelector(`.search-form`);
+    form.addEventListener(`submit`, onSearch);
+
+    initLightbox();
 });
 
 async function onSearch(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const query = event.target.elements.searchQuery.value.trim();
-  if (!query) {
-    showNotification(`Please enter a search query!`, `warning`);
-    return;
-  }
-
-  clearGallery();
-  showLoader();
-
-  try {
-    const images = await fetchImages(query);
-    hideLoader();
-
-    if (images.length === 0) {
-      showNotification(`Sorry, there are no images matching your search query. Please try again!`, `warning`);
-      return;
+    const query = event.target.elements.searchQuery.value.trim();
+    if (!query) {
+        showNotification(`Please enter a search query!`, `warning`);
+        return;
     }
 
-    renderImages(images);
-  } catch (error) {
-    hideLoader();
-    showNotification(`An error occurred while fetching images. Please try again!`, `error`);
-  }
+    clearGallery();
+    showLoader();
+
+    try {
+        const images = await fetchImages(query);
+        hideLoader();
+
+        if (images.length === 0) {
+            showNotification(`Sorry, there are no images matching your search query. Please try again!`, `warning`);
+            return;
+        }
+
+        renderImages(images);
+    } catch (error) {
+        hideLoader();
+        showNotification(`An error occurred while fetching images. Please try again!`, `error`);
+    }
 }
